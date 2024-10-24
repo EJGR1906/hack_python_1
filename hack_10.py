61428c97ae677042a3cba528ae356edbca5fcb31
EJGR1906 <eduardogonzalez1333@gmail.com>
2024-10-23 21:24:31 -0400

    Feature: Adding all Hacks

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,9 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
+
+    
     #...
-    return result  
+    return list(result.replace("o", "0").replace("i", "1").replace("a", "@").upper())
+
+print(fn_hack_10()) # ['f', '00', 'z', '1', 'm', '@', 'n']
--- hack_7.py
+++ hack_7.py
@@ -4,5 +4,8 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
+
+    for i in range(6):
+        result.append(5-i)
     #...
     return result  
